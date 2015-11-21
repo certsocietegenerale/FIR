1d8b43b58d356a47bb8b91904c3634c34cf8e08d
Bobby Argenbright <t3chm3chanics@gmail.com>
2015-11-21 09:02:44 -0800

    Added status field to Incidents serializer

--- fir/api/serializers.py
+++ fir/api/serializers.py
@@ -25,7 +25,7 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
 class IncidentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Incident
-        fields = ('date', 'is_starred', 'subject', 'description', 'severity', 'category', 'detection', 'opened_by', 'is_incident')
+        fields = ('date', 'is_starred', 'subject', 'description', 'severity', 'category', 'detection', 'opened_by', 'is_incident', 'status')
 
         def incident_list(request):
             """
