a2a0b4845f4665dee128621177188f599b2b4a99
Gaetan Crahay <gaetan@crahay.eu>
2016-01-21 10:48:06 +0100

    Improve IP regex in fir_artifacts

--- fir_artifacts/ip.py
+++ fir_artifacts/ip.py
@@ -4,4 +4,4 @@ from fir_artifacts.artifacts import AbstractArtifact
 class IP(AbstractArtifact):
 	key = 'ip'
 	display_name = 'IPS'
-	regex = r"(?P<search>[\d+]{1,3}\.[\d+]{1,3}\.[\d+]{1,3}\.[\d+]{1,3})"
+	regex = r'(([^\d])|^)(?P<search>(([2][5][0-5]\.)|([2][0-4][0-9]\.)|([0-1]?[0-9]?[0-9]\.)){3}(([2][5][0-5])|([2][0-4][0-9])|([0-1]?[0-9]?[0-9])))(([^\d]|$))'
