c4310405d4c3290d0ec1e409f1a879f0ef7bdd41
er587 <eric@123mail.org>
2016-04-02 10:25:26 -0400

    Update views.py

--- incidents/views.py
+++ incidents/views.py
@@ -514,10 +514,10 @@ def search(request):
             except Exception:
                 pass
 
-			opened_by = re.search("opened_by:(\S+)", query_string)
-			if opened_by:
-				q = q & Q(opened_by__username=opened_by.group(1))
-				query_string = query_string.replace('opened_by:'+opened_by.group(1), '')
+	    opened_by = re.search("opened_by:(\S+)", query_string)
+	    if opened_by:
+		q = q & Q(opened_by__username=opened_by.group(1))
+		query_string = query_string.replace('opened_by:'+opened_by.group(1), '')
 
             category = re.search("category:(\S+)", query_string)
             if category:
