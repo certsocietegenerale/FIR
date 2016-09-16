95c81f331ef06324dd5c9f3499559a16f1e7f98e
Thomas Chopitea <tomchop@gmail.com>
2016-09-16 18:13:51 +0200

    Fix #128

--- fir/wsgi.py
+++ fir/wsgi.py
@@ -9,6 +9,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fir.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fir.config.production")
 
 application = get_wsgi_application()
