f4876cca1ebf461cf705e55a17b1239dc79f1827
Gaetan Crahay <gaetan@crahay.eu>
2016-09-26 15:07:23 +0200

    Move content_type template filter from fir_artifacts to fir_plugins (used by fir_artifacts and fir_relations)

--- fir_plugins/templatetags/fir_plugins.py
+++ fir_plugins/templatetags/fir_plugins.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.utils.html import mark_safe
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
+from django.contrib.contenttypes.models import ContentType
 
 register = template.Library()
 
@@ -10,24 +11,29 @@ apps = settings.INSTALLED_APPS
 
 
 def template_path(app, name):
-	return '/'.join((app, 'plugins', name)) + '.html'
+    return '/'.join((app, 'plugins', name)) + '.html'
 
 
 @register.simple_tag(takes_context=True)
 def plugin_point(context, name):
-	templates = [template_path(app, name) for app in apps]
+    templates = [template_path(app, name) for app in apps]
 
-	result = ""
-	context = context.flatten()
-	for template in templates:
-		try:
-			t = get_template(template)
-			result += t.render(context, context['request'])
-		except TemplateDoesNotExist:
-			pass
-
-	return mark_safe(result)
+    result = ""
+    context = context.flatten()
+    for template in templates:
+        try:
+            t = get_template(template)
+            result += t.render(context, context['request'])
+        except TemplateDoesNotExist:
+            pass
+    return mark_safe(result)
 
 @register.filter
 def relation_name(obj):
-	return obj.__class__.__name__.lower()+'s'
+    return obj.__class__.__name__.lower()+'s'
+
+@register.filter
+def content_type(obj):
+    if not obj:
+        return False
+    return ContentType.objects.get_for_model(obj).pk
