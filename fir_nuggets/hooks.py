356978ad88b20727fb5456bff880dcd460341190
Gael Muller <muller.gael@gmail.com>
2016-12-07 14:25:54 +0100

    Code cleanup

--- fir_nuggets/hooks.py
+++ fir_nuggets/hooks.py
@@ -8,13 +8,11 @@ def keyword_filter(q, query_string):
         nugget = nugget.group(1)
         q = q & Q(nugget__source__icontains=nugget) | Q(nugget__raw_data__icontains=nugget) | Q(nugget__interpretation__icontains=nugget)
         query_string = query_string.replace('nugget:' + nugget, '')
-    print "keyword_filter", q, query_string
     return q, query_string
 
 
 def search_filter(q, query_string):
     q = q | (Q(nugget__source__icontains=query_string) | Q(nugget__raw_data__icontains=query_string) | Q(nugget__interpretation__icontains=query_string))
-    print "search_filter", query_string
     return q, query_string
 
 
