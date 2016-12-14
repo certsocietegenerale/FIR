d9b8778770815c4857204e1997c65d1099d5bf6f
rico <yemo.dasilva@gmail.com>
2016-12-14 19:44:17 +0100

    Change to Dockerization of fir docker-compose fir/redis/celery

--- fir/celery.py
+++ fir/celery.py
@@ -5,13 +5,17 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fir.settings')
 
+REDIS_HOST = os.environ.get('REDIS_PORT_6379_TCP_ADDR', 'redis')
+REDIS_PORT = 6379
+REDIS_DB = 0
+
 app = Celery('fir',
-        broker='redis://localhost:6379/0',
-        backend='redis://localhost:6379/0'
+        broker='redis://%s:%d/%d' % (REDIS_HOST, REDIS_PORT, REDIS_DB),
+        backend='redis://%s:%d/%d' % (REDIS_HOST, REDIS_PORT, REDIS_DB)
         )
 
 # load task module from all registered plugin
-#app.autodiscover_tasks(['fir.whois', 'fir_artifacts'])
+app.autodiscover_tasks()
 imports = ('fir.whois', 'fir.network_whois')
 
 if __name__ == '__main__':
