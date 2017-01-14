0d0bbced43de73d0f5f05dd3214ca378836d6c2f
Gaetan Crahay <gaetan@crahay.eu>
2017-01-14 12:28:13 +0100

    Notifications: event and method registry

---
+++ fir_notifications/registry.py
@@ -0,0 +1,34 @@
+from collections import OrderedDict
+
+
+class Notifications(object):
+    def __init__(self):
+        self.methods = OrderedDict()
+        self.events = OrderedDict()
+
+    def register_method(self, method, name=None, verbose_name=None):
+        if not method.server_configured:
+            return
+        if name is not None:
+            method.name = name
+        if verbose_name is not None:
+            method.verbose_name = verbose_name
+        if not method.verbose_name:
+            method.verbose_name = method.name
+        self.methods[method.name] = method
+
+    def register_event(self, name, signal, model, callback, verbose_name=None):
+        if verbose_name is None:
+            verbose_name = name
+        self.events[name] = verbose_name
+
+        signal.connect(callback, sender=model, dispatch_uid="fir_notifications.{}".format(name))
+
+    def get_event_choices(self):
+        return self.events.items()
+
+    def get_method_choices(self):
+        return [(obj.name, obj.verbose_name) for obj in self.methods.values()]
+
+
+registry = Notifications()
