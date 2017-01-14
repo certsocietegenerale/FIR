f03a98e8ba699e4d1243e502dba8eef421697968
Gaetan Crahay <gaetan@crahay.eu>
2017-01-14 21:22:39 +0100

    Notifications: improve event registry

--- fir_notifications/registry.py
+++ fir_notifications/registry.py
@@ -1,6 +1,24 @@
 from collections import OrderedDict
 
+from django.apps import apps
+from django.utils.encoding import python_2_unicode_compatible
+
 from fir_notifications.methods.email import EmailMethod
+
+
+@python_2_unicode_compatible
+class RegisteredEvent(object):
+    def __init__(self, name, model, verbose_name=None, section = None):
+        self.name = name
+        if section is None:
+            section = apps.get_app_config(model._meta.app_label).verbose_name
+        self.section = section
+        if verbose_name is None:
+            verbose_name = name
+        self.verbose_name = verbose_name
+
+    def __str__(self):
+        return self.verbose_name
 
 
 class Notifications(object):
@@ -19,15 +37,15 @@ class Notifications(object):
             method.verbose_name = method.name
         self.methods[method.name] = method
 
-    def register_event(self, name, signal, model, callback, verbose_name=None):
+    def register_event(self, name, signal, model, callback, verbose_name=None, section=None):
         if verbose_name is None:
             verbose_name = name
-        self.events[name] = verbose_name
+        self.events[name] = RegisteredEvent(name, model, verbose_name=verbose_name, section=section)
 
         signal.connect(callback, sender=model, dispatch_uid="fir_notifications.{}".format(name))
 
     def get_event_choices(self):
-        return self.events.items()
+        return [(obj.name, obj.verbose_name) for obj in self.events.values()]
 
     def get_method_choices(self):
         return [(obj.name, obj.verbose_name) for obj in self.methods.values()]
