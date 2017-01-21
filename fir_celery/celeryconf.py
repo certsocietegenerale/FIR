599bcba6864e37377651c9193468b6af17545ef2
Rico <yemo.dasilva@gmail.com>
2017-01-21 18:46:30 +0100

    Minor refactor celeryconf
     Use of autodiscover_tasks native to celery

--- fir_celery/celeryconf.py
+++ fir_celery/celeryconf.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 
 import os
-from pkgutil import find_loader
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fir.settings')
@@ -9,19 +8,15 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fir.settings')
 from django.conf import settings
 
 
-includes = []
-for app in settings.INSTALLED_APPS:
-    if app.startswith('fir_'):
-        module_name = "{}.tasks".format(app)
-        if find_loader(module_name):
-            includes.append(module_name)
-
 celery_app = Celery(
     'celeryconf',
-    broker='redis://%s:%d/%d' % (settings.REDIS_HOST, settings.REDIS_PORT, settings.REDIS_DB),
-    backend='redis://%s:%d/%d' % (settings.REDIS_HOST, settings.REDIS_PORT, settings.REDIS_DB),
-    include=includes
-)
+    broker='redis://%s:%d/%d' % (settings.REDIS_HOST, settings.REDIS_PORT,
+        settings.REDIS_DB),
+    backend='redis://%s:%d/%d' % (settings.REDIS_HOST, settings.REDIS_PORT,
+        settings.REDIS_DB)
+    )
+
+celery_app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
 if __name__ == '__main__':
