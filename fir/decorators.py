ed5d6610a8837a6e8ae51103d50e6b4f2fbd8666
Fred <udgover@digital-forensic.org>
2018-01-19 10:28:05 +0100

    Fixes import error when two_factor is not installed

--- fir/decorators.py
+++ fir/decorators.py
@@ -3,12 +3,12 @@ from functools import wraps
 
 from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.contrib.auth.decorators import login_required
-from django_otp.decorators import otp_required
 from fir.config.base import TF_INSTALLED, ENFORCE_2FA
 
 
 def fir_auth_required(view=None, redirect_field_name=None, login_url=None):
     if TF_INSTALLED:
+        from django_otp.decorators import otp_required
         if ENFORCE_2FA:
             decorator = otp_required(view=view, redirect_field_name=REDIRECT_FIELD_NAME, login_url=login_url, if_configured=False)
         else:
