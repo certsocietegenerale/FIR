47a9d3581ba7e3bb806a1e291496b99c016cf154
Nicholas de Jong <contact@nicholasdejong.com>
2019-03-03 12:56:06 +0800

    implements DEFAULT_PAGINATION_CLASS for the rest_framework as is now required for this version

--- fir_api/settings.py
+++ fir_api/settings.py
@@ -2,5 +2,6 @@
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.TokenAuthentication', 'rest_framework.authentication.SessionAuthentication'),
-    'PAGE_SIZE': 10
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10,
 }
