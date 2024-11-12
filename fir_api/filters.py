ed7a84ed869421e7be3f4b36cdb3e88f4451e31d
Augustin-FL <Augustin-FL@users.noreply.github.com>
2024-11-12 14:23:15 +0100

    Filter by name instead of IDs

--- fir_api/filters.py
+++ fir_api/filters.py
@@ -13,6 +13,7 @@ from django_filters.rest_framework import (
 from incidents.models import (
     Incident,
     Label,
+    LabelGroup,
     IncidentCategory,
     Comments,
     File,
@@ -125,6 +126,11 @@ class LabelFilter(FilterSet):
 
     id = NumberFilter(field_name="id")
     name = CharFilter(field_name="name")
+    group = ModelChoiceFilter(
+        to_field_name="name",
+        field_name="group__name",
+        queryset=LabelGroup.objects.all(),
+    )
     color = CharFilter(
         field_name="dynamic_config__color", lookup_expr="icontains", label="color"
     )
