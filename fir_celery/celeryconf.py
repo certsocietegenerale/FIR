ea231e2eb92e42178c71a4e14fbe986852e70d07
Augustin-FL <Augustin-FL@users.noreply.github.com>
2024-12-28 19:45:12 +0100

    Remove warnings on startup

--- fir_celery/celeryconf.py
+++ fir_celery/celeryconf.py
@@ -3,21 +3,21 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fir.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fir.settings")
 
 from django.conf import settings
 
 
 celery_app = Celery(
-    'celeryconf',
-    broker='redis://%s:%d/%d' % (settings.REDIS_HOST, settings.REDIS_PORT,
-        settings.REDIS_DB),
-    backend='redis://%s:%d/%d' % (settings.REDIS_HOST, settings.REDIS_PORT,
-        settings.REDIS_DB)
-    )
+    "celeryconf",
+    broker="redis://%s:%d/%d"
+    % (settings.REDIS_HOST, settings.REDIS_PORT, settings.REDIS_DB),
+    backend="redis://%s:%d/%d"
+    % (settings.REDIS_HOST, settings.REDIS_PORT, settings.REDIS_DB),
+    broker_connection_retry_on_startup=True,
+)
 
 celery_app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     celery_app.start()
