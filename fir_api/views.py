88e6c1d40d0628bc664e80188542d364fd89b3dc
Augustin-FL <Augustin-FL@users.noreply.github.com>
2025-03-07 17:01:17 +0100

    Allow to sort by confidentiality, severity, actor, category

--- fir_api/views.py
+++ fir_api/views.py
@@ -116,6 +116,10 @@ class IncidentViewSet(
         "subject",
         "concerned_business_lines",
         "last_comment_date",
+        "category",
+        "severity",
+        "confidentiality",
+        "actor",
     ]
     filterset_class = IncidentFilter
 
