a64c6995984b6f723f74ccb36a847cfe8e592995
Augustin-FL <Augustin-FL@users.noreply.github.com>
2025-03-10 16:27:24 +0100

    Use API endpoints when displaying dashboards, listing or searching incidents

--- fir_nuggets/hooks.py
+++ fir_nuggets/hooks.py
@@ -1,36 +1,20 @@
 import re
-
 from django.db.models import Q
 
+from fir_nuggets.api import NuggetSerializer
 
-def keyword_filter(q, query_string):
-    nugget = re.search("nugget:(\S+)", query_string)
-    if nugget:
-        nugget = nugget.group(1)
-        q = (
-            q & Q(nugget__source__icontains=nugget)
-            | Q(nugget__raw_data__icontains=nugget)
-            | Q(nugget__interpretation__icontains=nugget)
-        )
-        query_string = query_string.replace("nugget:" + nugget, "")
-    return q, query_string
-
-
-def search_filter(q, query_string):
-    q = q | (
-        Q(nugget__source__icontains=query_string)
-        | Q(nugget__raw_data__icontains=query_string)
-        | Q(nugget__interpretation__icontains=query_string)
-    )
-    return q, query_string
-
-
-try:
-    from fir_nuggets.api import NuggetSerializer
-except ModuleNotFoundError:
-    incident_fields = {}
-else:
-    incident_fields = [
+hooks = {
+    "keyword_filter": {
+        "nugget": lambda x: Q(nugget__source__icontains=x)
+        | Q(nugget__raw_data__icontains=x)
+        | Q(nugget__interpretation__icontains=x)
+    },
+    "search_filter": [
+        lambda x: Q(nugget__source__icontains=x)
+        | Q(nugget__raw_data__icontains=x)
+        | Q(nugget__interpretation__icontains=x)
+    ],
+    "incident_fields": [
         (
             "nugget_set",  #  name of the new field
             None,
@@ -39,10 +23,5 @@ else:
             ),  # Serializer corresponding to the new field
             None,
         ),
-    ]
-
-hooks = {
-    "keyword_filter": keyword_filter,
-    "search_filter": search_filter,
-    "incident_fields": incident_fields,
+    ],
 }
