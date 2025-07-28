830d29dbf6e2dffe46495db5b67f5f516bc39751
Augustin-FL <Augustin-FL@users.noreply.github.com>
2025-07-28 16:40:58 +0200

    Require phone numbers to start with a + This helps avoiding false positive

--- fir_artifacts/phone.py
+++ fir_artifacts/phone.py
@@ -6,7 +6,7 @@ class Phone(AbstractArtifact):
     key = "phone"
     display_name = "Phone Numbers"
     regex = r"""(?P<search>
-        (?:(?:\+|00)\d{1,3}|0)                     # country code or "0"
+        \+\d{1,3}                                  # country code
         [ \t.\-()]*                                # optional separators (no newlines)
         (?:\(?\d{1,4}\)?[ \t.\-()]*)?              # optional area code
         \d{1,4}                                    # main number start
@@ -19,16 +19,13 @@ class Phone(AbstractArtifact):
         _re = re.compile(cls.regex, re.VERBOSE)
         for match in re.finditer(_re, data):
             raw = match.group("search")
-            # remove (0) if the number starts with + or 00
-            if raw.strip().startswith(("+", "00")):
-                raw = raw.replace("(0)", "")
+            # remove (0) if any
+            raw = raw.replace("(0)", "")
             # remove common separators
             cleaned = re.sub(r"[()\t \xa0\-\.]", "", raw)
-            # 00 -> +
-            if cleaned.startswith("00"):
-                cleaned = "+" + cleaned[2:]
             digits_only = re.sub(r"\D", "", cleaned)
             if len(digits_only) < 9 or len(digits_only) > 15:
-              continue
+                # Invalid phone numbers
+                continue
             results.append(cleaned)
         return results
