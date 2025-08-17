6149d440928de3a1aad4badb04886d1928f79efa
Augustin-FL <Augustin-FL@users.noreply.github.com>
2025-08-17 13:01:16 +0200

    migrate to the elastic fork of handlebars as the vanilla version is not compatible with a strict CSP See https://github.com/handlebars-lang/handlebars.js/issues/1934
    The following command were used to generate handlebars.elastic.min.js:
    git clone https://github.com/elastic/handlebars.git
    cd handlebars
    npm install
    npx esbuild --bundle src/handlebars.ts --platform=browser --global-name=Handlebars --format=iife --minify --footer:js="window.Handlebars = (Handlebars && Handlebars.Handlebars) ? Handlebars.Handlebars : Handlebars;" --outfile=dist/handlebars.elastic.min.js

--- incidents/static/custom_js/handlebars_custom_syntax.js
+++ incidents/static/custom_js/handlebars_custom_syntax.js
@@ -1,6 +1,7 @@
 // Patch handlebars: Convert ${...} to {{...}}
-const hb_originalCompile = Handlebars.compile;
+// Then use compileAST from https://github.com/elastic/handlebars
+// To allow handlebars usage when having a strict CSP
 Handlebars.compile = function (templateStr, options) {
   const converted = templateStr.replace(/\$\{([^}]+)\}/g, "{{$1}}");
-  return hb_originalCompile.call(this, converted, options);
+  return Handlebars.compileAST(converted, options);
 };
