7cf30dc8e6708d4f3c2f7589818d719cd7272726
Augustin-FL <Augustin-FL@users.noreply.github.com>
2025-12-07 10:16:25 +0100

    Restore custom pagination It was removed by mistake in 686314c

--- fir_api/pagination.py
+++ fir_api/pagination.py
@@ -1,9 +1,19 @@
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
+from rest_framework.settings import api_settings
 
 
 class CustomPageNumberPagination(PageNumberPagination):
     page_size_query_param = "page_size"
+
+    def get_page_size(self, request):
+        self.page_size = api_settings.PAGE_SIZE
+        if hasattr(request.user, "profile") and hasattr(
+            request.user.profile, "incident_number"
+        ):
+            self.page_size = request.user.profile.incident_number
+
+        return super().get_page_size(request)
 
     # Add "total_pages" to the response
     def get_paginated_response(self, data):
